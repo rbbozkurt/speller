dad45df7cc100b57894a41e9a36b32cd03b2803e
rbbozkurt <resit.b.bozkurt@campus.tu-berlin.de>
2023-06-07 15:17:19 +0200

    last succesfull re-run

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='speller',
+    version='',
+    packages=['plot', 'train', 'database'],
+    url='',
+    license='',
+    author='resitberkaybozkurt',
+    author_email='',
+    description=''
+)
